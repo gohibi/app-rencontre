d1d648d1aaed74a5f42c48432909e537aa960e1e
gohibi <andrejoelgohi@gmail.com>
2025-04-30 15:36:58 +0000

    [flutter_releases] Flutter stable 3.29.0 engine version change  (#165139)
    Only the engine version change.

--- lib/homeScreen/home_screen.dart
+++ lib/homeScreen/home_screen.dart
@@ -32,7 +32,6 @@ class _HomeScreenState extends State<HomeScreen> {
   }
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     PushNotificationSystem pushNotificationSystem = PushNotificationSystem();
     pushNotificationSystem.generateDeviceRegistrationToken();
